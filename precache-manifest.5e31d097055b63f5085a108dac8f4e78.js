7ba4c3c0ae5c2543d333b89bd82dd383c65f9177
j0nnc0der <jonlondontech@gmail.com>
2019-12-24 15:19:01 +0000

    Updates

---
+++ precache-manifest.5e31d097055b63f5085a108dac8f4e78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc890d060a1158d56423f2186612a59",
+    "url": "/Newreactapp/index.html"
+  },
+  {
+    "revision": "cb6e2164c16e3836e191",
+    "url": "/Newreactapp/static/css/main.8c01bf6c.chunk.css"
+  },
+  {
+    "revision": "5ef74deaf4844c3286a0",
+    "url": "/Newreactapp/static/js/2.b35eece8.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/Newreactapp/static/js/2.b35eece8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb6e2164c16e3836e191",
+    "url": "/Newreactapp/static/js/main.421ff6fd.chunk.js"
+  },
+  {
+    "revision": "e51caa80dcca7c03cad9",
+    "url": "/Newreactapp/static/js/runtime-main.1fbb12ab.js"
+  }
+]);
